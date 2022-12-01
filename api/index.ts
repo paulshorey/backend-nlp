8f935387f5dc3295d7c96f7d4d9fc03a44a06e36
Paul Shorey <web@artspaces.net>
2022-11-30 21:35:41 -0600

    test deploy as submodule

--- api/index.ts
+++ api/index.ts
@@ -98,7 +98,7 @@ expressApp.get("/", async function (req, res) {
     "hello there"
   ]
   http_response(res, 200, {
-    v: "1",
+    v: "2",
     time: Date.now(),
     randomGreeting: greetings[Math.floor(Math.random() * 22)]
     // message: "Please see https://besta.domains/api for API documentation."
